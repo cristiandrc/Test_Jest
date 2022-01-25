d40c9e357dad3bd4d37e294e339626222a8a631e
cristian <crisdarroca@hotmail.com>
2022-01-24 19:56:32 -0500

    add snapshots

--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import ProviderMock from '../../__mocks__/ProviderMock';
 import Header from '../../components/Header';
+import { create } from 'react-test-renderer';
 
 describe('<Header/>', () => {
   test('render del componente header', () => {
@@ -21,5 +22,17 @@ describe('<Header/>', () => {
       </ProviderMock>
     );
     expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+//para actualizar los snapshot ejecutamos jest --updateSnapshot
+describe('Header snapShot', () => {
+  test('comprobar el snapshot de header ', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.toJSON()).toMatchSnapshot();
   });
 });
