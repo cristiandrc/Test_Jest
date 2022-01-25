54207de468373a7d07f3fec386d79d292f578932
cristian <crisdarroca@hotmail.com>
2022-01-24 20:10:47 -0500

    test actions

---
+++ src/__test__/actions/actions.test.js
@@ -0,0 +1,23 @@
+import actions from '../../actions/';
+import ProductMock from '../../__mocks__/ProductMock';
+
+describe('Actions', () => {
+  const payload = ProductMock;
+  test('addToCart Action', () => {
+    const expected = {
+      type: 'ADD_TO_CART',
+      payload,
+    };
+
+    expect(actions.addToCart(payload)).toEqual(expected);
+  });
+
+  test('removeCart actions', () => {
+    const expected = {
+      type: 'REMOVE_FROM_CART',
+      payload,
+    };
+
+    expect(actions.removeFromCart(payload)).toEqual(expected);
+  });
+});
