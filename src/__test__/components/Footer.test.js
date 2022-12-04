f3b63af77c043ccbb9fd94c340241e3b0086b4c4
Cristian Rojas <crisdarroca@hotmail.com>
2022-12-04 12:40:25 -0500

    moock of styles

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -3,8 +3,13 @@ import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer/>', () => {
+  const footer = mount(<Footer />);
+
   test('Component Render Footer', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
+  });
+
+  test('H1 Render', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
   });
 });
